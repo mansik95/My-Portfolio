9e5f524fc09c03085944719dbba5e06b163d0ed2
Mansi Pandya <mansipandya@Mansis-MacBook-Pro.local>
2020-07-01 16:34:23 -0400

    Uploading portfolio to git

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $('#slides').superslides();
+
+    var typed = new Typed(".typed", {
+        strings: ["Student", "Data Analyst", "Business Intelligence Developer"],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+
+
+});
